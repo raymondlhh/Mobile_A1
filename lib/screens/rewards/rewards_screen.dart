513633d4c018015c0472e367fe2d0e23d10f0173
Dexterong9 <dexter.ong6@gmail.com>
2025-05-12 18:44:23 +0800

    Update rewards_screen.dart

--- lib/screens/rewards/rewards_screen.dart
+++ lib/screens/rewards/rewards_screen.dart
@@ -1,16 +1,14 @@
 // lib/screens/home/rewards_screen.dart
+//test
 import 'package:flutter/material.dart';
 
 class RewardsScreen extends StatelessWidget {
-  const RewardsScreen({super.key});  // FIX key constructor
+  const RewardsScreen({super.key}); // FIX key constructor
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'Rewards Screen',
-        style: TextStyle(fontSize: 24),
-      ),
+      child: Text('Rewards Screen', style: TextStyle(fontSize: 24)),
     );
   }
 }
