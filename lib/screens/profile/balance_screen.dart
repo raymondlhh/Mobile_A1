7ed3f2a2ecf367a5da26c4ebc50c30059eedc86a
Raymond <88035771+Youcha12345@users.noreply.github.com>
2025-05-13 10:42:42 +0800

    balance_screen.dart

--- lib/screens/profile/balance_screen.dart
+++ lib/screens/profile/balance_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/title_appbar.dart';
+
 class BalanceScreen extends StatelessWidget {
   const BalanceScreen({super.key});
 
@@ -7,16 +9,24 @@ class BalanceScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFFF8E5),
-      appBar: AppBar(
-        backgroundColor: const Color(0xFFFFF8E5),
-        elevation: 0,
-        title: const Text(
-          'Balance',
-          style: TextStyle(fontFamily: 'GaMaamli', color: Colors.black),
+      appBar: buildAppBar(context, 'Balance', actionType: AppBarActionType.none),
+      body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                children: [
+                  Image.asset(
+                    'assets/images/others/InkPainting.png',
+                    width: double.infinity,
+                    height: 160,
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        centerTitle: true,
-      ),
-      body: const Center(child: Text('Balance Screen')),
-    );
+      );
+    }
   }
-}
