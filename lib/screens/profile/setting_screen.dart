08d4e9a7ccbe8ef0219c9fdcd5fe03356a14d15b
Raymond <88035771+Youcha12345@users.noreply.github.com>
2025-05-13 17:24:47 +0800

    Profile Done

--- lib/screens/profile/setting_screen.dart
+++ lib/screens/profile/setting_screen.dart
@@ -1,22 +1,64 @@
 import 'package:flutter/material.dart';
+import '../../widgets/title_appbar.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  bool _isNotificationOn = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFFF8E5),
-      appBar: AppBar(
-        backgroundColor: const Color(0xFFFFF8E5),
-        elevation: 0,
-        title: const Text(
-          'Setting',
-          style: TextStyle(fontFamily: 'GaMaamli', color: Colors.black),
+      appBar: buildAppBar(context, 'SETTINGS', actionType: AppBarActionType.none),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Privacy',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w900,
+                fontFamily: 'Inter',
+              ),
+            ),
+            const SizedBox(height: 0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Push Notification',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Inter',
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _isNotificationOn = !_isNotificationOn;
+                    });
+                  },
+                  child: Image.asset(
+                    _isNotificationOn
+                        ? 'assets/images/buttons/OnSwitch.png'
+                        : 'assets/images/buttons/OffSwitch.png',
+                    width: 40,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
-        centerTitle: true,
       ),
-      body: const Center(child: Text('Setting Screen')),
     );
   }
 }
