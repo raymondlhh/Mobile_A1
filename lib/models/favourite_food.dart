926f9816db3b45c48516a33a6ddea954c7ba7476
Raymond <88035771+Youcha12345@users.noreply.github.com>
2025-05-13 16:23:14 +0800

    favourite_food

--- lib/models/favourite_food.dart
+++ lib/models/favourite_food.dart
@@ -37,5 +37,68 @@ List<FavouriteFood> favouriteFoods = [
     imagePath: 'assets/images/foods/partySet/GunkanSet26pcs.png',
   ),
 
+  FavouriteFood(
+    name: 'Chuka Idako',
+    rating: 3.8,
+    totalRating: '118+ Rating',
+    imagePath: 'assets/images/foods/appetizer/ChukaIdako.png',
+  ),
+
+  FavouriteFood(
+    name: 'Sushi Set 1',
+    rating: 4.5,
+    totalRating: '200+ Rating',
+    imagePath: 'assets/images/foods/moriawaseMakimonoSet/SushiSet1.png',
+  ),
+
+  FavouriteFood(
+    name: 'Gunkan Set (26 pcs)',
+    rating: 4.8,
+    totalRating: '230+ Rating',
+    imagePath: 'assets/images/foods/partySet/GunkanSet26pcs.png',
+  ),
+
+  FavouriteFood(
+    name: 'Chuka Idako',
+    rating: 3.8,
+    totalRating: '118+ Rating',
+    imagePath: 'assets/images/foods/appetizer/ChukaIdako.png',
+  ),
+
+  FavouriteFood(
+    name: 'Sushi Set 1',
+    rating: 4.5,
+    totalRating: '200+ Rating',
+    imagePath: 'assets/images/foods/moriawaseMakimonoSet/SushiSet1.png',
+  ),
+
+  FavouriteFood(
+    name: 'Gunkan Set (26 pcs)',
+    rating: 4.8,
+    totalRating: '230+ Rating',
+    imagePath: 'assets/images/foods/partySet/GunkanSet26pcs.png',
+  ),
+
+  FavouriteFood(
+    name: 'Chuka Idako',
+    rating: 3.8,
+    totalRating: '118+ Rating',
+    imagePath: 'assets/images/foods/appetizer/ChukaIdako.png',
+  ),
+
+  FavouriteFood(
+    name: 'Sushi Set 1',
+    rating: 4.5,
+    totalRating: '200+ Rating',
+    imagePath: 'assets/images/foods/moriawaseMakimonoSet/SushiSet1.png',
+  ),
+
+  FavouriteFood(
+    name: 'Gunkan Set (26 pcs)',
+    rating: 4.8,
+    totalRating: '230+ Rating',
+    imagePath: 'assets/images/foods/partySet/GunkanSet26pcs.png',
+  ),
+
   
 ];
