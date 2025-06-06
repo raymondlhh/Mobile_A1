c13eb46158f7636b0e0170003acb3188920af6cb
Raymond <88035771+Youcha12345@users.noreply.github.com>
2025-06-06 10:27:34 +0800

    Test

--- lib/screens/profile/favourite_screen.dart
+++ lib/screens/profile/favourite_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../../models/favourite_food.dart';
 import '../../widgets/title_appbar.dart';
 import '../../widgets/favourite_food_card.dart';
+import '../../services/database_helper.dart';
 
 class FavouriteScreen extends StatefulWidget {
   const FavouriteScreen({super.key});
@@ -11,30 +12,48 @@ class FavouriteScreen extends StatefulWidget {
 }
 
 class _FavouriteScreenState extends State<FavouriteScreen> {
-  // Start with reversed list: newest on top
-  // ignore: prefer_final_fields
-  List<FavouriteFood> _foods = List.from(favouriteFoods.reversed);
+  final DatabaseHelper _dbHelper = DatabaseHelper.instance;
+  List<FavouriteFood> _foods = [];
 
-  void _removeFromFavourites(FavouriteFood food) {
+  @override
+  void initState() {
+    super.initState();
+    _loadFavorites();
+  }
+
+  Future<void> _loadFavorites() async {
+    final favorites = await _dbHelper.getAllFavorites();
     setState(() {
-      _foods.remove(food); // this will cause the items below to move up
+      _foods = favorites;
     });
+  }
+
+  Future<void> _removeFromFavourites(FavouriteFood food) async {
+    await _dbHelper.delete(food.name);
+    await _loadFavorites(); // Reload the list after deletion
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(context, 'Favourites', actionType: AppBarActionType.none),
-      body: ListView.builder(
-        itemCount: _foods.length,
-        padding: const EdgeInsets.all(22),
-        itemBuilder: (context, index) {
-          return FavouriteFoodCard(
-            food: _foods[index],
-            onRemove: () => _removeFromFavourites(_foods[index]),
-          );
-        },
-      ),
+      body: _foods.isEmpty
+          ? const Center(
+              child: Text(
+                'No favorites yet',
+                style: TextStyle(fontSize: 18),
+              ),
+            )
+          : ListView.builder(
+              itemCount: _foods.length,
+              padding: const EdgeInsets.all(22),
+              itemBuilder: (context, index) {
+                return FavouriteFoodCard(
+                  food: _foods[index],
+                  onRemove: () => _removeFromFavourites(_foods[index]),
+                );
+              },
+            ),
     );
   }
 }
