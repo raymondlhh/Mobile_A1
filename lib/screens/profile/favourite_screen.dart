49af343175cb674e76ed9099bf203903569ba546
Dexterong9 <dexter.ong6@gmail.com>
2025-06-15 14:46:54 +0800

    remove sqlite

--- lib/screens/profile/favourite_screen.dart
+++ lib/screens/profile/favourite_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import '../../models/favourite_food.dart';
 import '../../widgets/title_appbar.dart';
 import '../../widgets/favourite_food_card.dart';
-import '../../services/database_helper.dart';
 
 class FavouriteScreen extends StatefulWidget {
   const FavouriteScreen({super.key});
@@ -12,7 +11,6 @@ class FavouriteScreen extends StatefulWidget {
 }
 
 class _FavouriteScreenState extends State<FavouriteScreen> {
-  final DatabaseHelper _dbHelper = DatabaseHelper.instance;
   List<FavouriteFood> _foods = [];
 
   @override
@@ -22,38 +20,36 @@ class _FavouriteScreenState extends State<FavouriteScreen> {
   }
 
   Future<void> _loadFavorites() async {
-    final favorites = await _dbHelper.getAllFavorites();
-    setState(() {
-      _foods = favorites;
-    });
+    setState(() {});
   }
 
   Future<void> _removeFromFavourites(FavouriteFood food) async {
-    await _dbHelper.delete(food.name);
     await _loadFavorites(); // Reload the list after deletion
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(context, 'Favourites', actionType: AppBarActionType.none),
-      body: _foods.isEmpty
-          ? const Center(
-              child: Text(
-                'No favorites yet',
-                style: TextStyle(fontSize: 18),
+      appBar: buildAppBar(
+        context,
+        'Favourites',
+        actionType: AppBarActionType.none,
+      ),
+      body:
+          _foods.isEmpty
+              ? const Center(
+                child: Text('No favorites yet', style: TextStyle(fontSize: 18)),
+              )
+              : ListView.builder(
+                itemCount: _foods.length,
+                padding: const EdgeInsets.all(22),
+                itemBuilder: (context, index) {
+                  return FavouriteFoodCard(
+                    food: _foods[index],
+                    onRemove: () => _removeFromFavourites(_foods[index]),
+                  );
+                },
               ),
-            )
-          : ListView.builder(
-              itemCount: _foods.length,
-              padding: const EdgeInsets.all(22),
-              itemBuilder: (context, index) {
-                return FavouriteFoodCard(
-                  food: _foods[index],
-                  onRemove: () => _removeFromFavourites(_foods[index]),
-                );
-              },
-            ),
     );
   }
 }
