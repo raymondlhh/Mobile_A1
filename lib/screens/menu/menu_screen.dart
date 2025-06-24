52546cbbba0b543c5662aaf11c9369e8f4ee6001
johnygan23 <gan.siongbin@gmail.com>
2025-06-25 00:03:19 +0800

    Add shopping cart functionality and navigation to Shopping Cart

--- lib/screens/menu/menu_screen.dart
+++ lib/screens/menu/menu_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/menu/shopping_cart.dart';
 import '../../data/menu_data.dart';
 import 'item_detail_page.dart';
 import '../../models/menu_item.dart';
@@ -180,7 +181,12 @@ class _MenuScreenState extends State<MenuScreen> {
                       size: 30,
                     ),
                     onPressed: () {
-                      // TODO: Implement shopping cart functionality
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ShoppingCart(),
+                        ),
+                      );
                     },
                   ),
                 ],
