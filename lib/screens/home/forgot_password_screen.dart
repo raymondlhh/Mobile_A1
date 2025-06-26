769a4ff565d1ef29db3e6ff088176599fbfd06e6
johnygan23 <gan.siongbin@gmail.com>
2025-06-26 22:33:40 +0800

    Remove unused Firebase Auth import
    Deleted the unused import of firebase_auth from forgot_password_screen.dart to clean up the code.

--- lib/screens/home/forgot_password_screen.dart
+++ lib/screens/home/forgot_password_screen.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: deprecated_member_use, unused_field
 
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
