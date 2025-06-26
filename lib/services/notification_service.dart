60ca58f29f0af098c9101714838bcf32c1047ed4
Raymond <88035771+Youcha12345@users.noreply.github.com>
2025-06-26 14:21:16 +0800

    Revert "notification"
    This reverts commit 2a0d422f0e6b8d201f393b7176767e211f511c9f.

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/foundation.dart';
-import 'local_notification_helper.dart';
 
 class NotificationItem {
   final String name;
@@ -56,10 +55,6 @@ class NotificationService extends ChangeNotifier {
     notifications.insert(0, notification); // newest first
     await saveNotifications(notifications);
     notifyListeners();
-    await LocalNotificationHelper.showNotification(
-      title: notification.name,
-      body: notification.description,
-    );
   }
 
   Future<void> markAllAsRead() async {
