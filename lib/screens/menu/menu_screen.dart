360c20d586de7d03ba8729dbaa3882c37fba3ce5
johnygan23 <gan.siongbin@gmail.com>
2025-06-28 14:56:46 +0800

    Update menu_screen.dart
    Fixed keyboard status while typing

--- lib/screens/menu/menu_screen.dart
+++ lib/screens/menu/menu_screen.dart
@@ -270,7 +270,6 @@ class _MenuScreenState extends State<MenuScreen> {
                     ),
                     onChanged: _filterMenuItems,
                     onSubmitted: _filterMenuItems,
-                    onTapOutside: (_) => FocusScope.of(context).unfocus(),
                   ),
                 ),
                 // Horizontal Divider
